afc2d9c1a37e11081f4df42114896b9e56d05016
Prish20 <prish@students.uonbi.ac.ke>
2024-04-03 02:27:24 +0300

    correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-# Using puppet to make changes to the SSH config file
+#Puppet client congiguration
 
-file {'etc/ssh/ssh_config':
-    ensure => present,
-content => "
-# This is the ssh client system-wide configuration file.
-# See ssh_config(5) for more information. This file provides defaults for
-# users, and the values can be changed in per-user configuration files
-# or on the command line.
-    host*
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
+file {"etc/ssh/ssh_config":
+	ensure => present,
+content =>"
+	#Client ssh config
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
 ",
 }
