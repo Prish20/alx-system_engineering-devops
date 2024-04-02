e203618b38e0d50923189f9171c219a472eabf6a
Prish20 <prish@students.uonbi.ac.ke>
2024-04-03 02:36:26 +0300

    FIx

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 #Puppet client congiguration
 
-package { 'openssh-client':
-  ensure => installed,
-}
-
-file {'ect/ssh/ssh_config':
+file {'etc/ssh/ssh_config':
 	ensure => present,
 	content =>"
-	#Client ssh config
 	host*
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
