324ed13d9a95b678ec4b0de5bed85889894911db
Prish20 <prish@students.uonbi.ac.ke>
2024-05-07 23:05:32 +0300

    Updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,26 +7,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API to get the number
-    of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers for
-        the subreddit, or 0 if the subreddit is invalid.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "subreddit-subscriber-checker"}
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        return 0
-
-    try:
+    if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
-    except KeyError:
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
