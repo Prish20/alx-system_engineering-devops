f9d90df7675913bff5b26e50c772bd85588b7acc
Prish20 <prish@students.uonbi.ac.ke>
2024-05-07 22:50:11 +0300

    FIx PEP8 compliance

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API to get the number of subscribers for a given subreddit.
+    Queries the Reddit API to get the number
+    of subscribers for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit.
 
     Returns:
-        int: The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
+        int: The number of subscribers for
+        the subreddit, or 0 if the subreddit is invalid.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "subreddit-subscriber-checker"}
